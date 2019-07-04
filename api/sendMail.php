9803d1b83afea2f1b360cf89e193b08c3a1e107e
Yosk <da_afo_w@mail.ru>
2019-07-04 19:08:11 +0300

    Почта

--- api/sendMail.php
+++ api/sendMail.php
@@ -6,7 +6,7 @@ error_reporting(0);
 try {
     header('Content-type: application/json');
 
-    $to = 'yoskutik@gmail.com, tatiana.mix.1910@gmail.com, da_afo_w@mail.ru, tatiana@myinspire-ph.ru';
+    $to = '<yoskutik@gmail.com>, <tatiana.mix.1910@gmail.com>, <da_afo_w@mail.ru>, <tatiana@myinspire-ph.ru>';
     $subject = $_POST['subject'];
     $body = HTML::render('mail.html', [
             'FROM_NAME' => $_POST['name'],
@@ -16,12 +16,14 @@ try {
             'YEAR' => date('Y'),
         ],
         'api/patterns');
-    $headers = "From: {$_POST['name']} <{$_POST['email']}>" . "\r\n" .
-        "Reply-To: {$_POST['email']}" . "\r\n" .
-        'Content-type: text/html' . "\r\n" .
+    $headers = "From: Татьяна <tatiana@myinspire-ph.ru>" . "\r\n" .
+        "Reply-To: <tatiana@myinspire-ph.ru>" . "\r\n" .
+        'Content-type: text/html; charset=UTF-8' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 
     $status = mail($to, $subject, $body, $headers);
+
+    mail($to, $subject, $body, "From: {$_POST['name']} <{$_POST['email']}>\r\nReply-To: <{$_POST['email']}>\r\nContent-type: text/html; charset=UTF-8\r\nX-Mailer: PHP/" . phpversion());
 } catch (Error $e) {
     if ($e) $status = !!$e;
 }
@@ -29,3 +31,4 @@ try {
 if (!$status) http_response_code(400);
 
 echo json_encode(['status' => $status ? 'OK' : 'ERROR']);
+
